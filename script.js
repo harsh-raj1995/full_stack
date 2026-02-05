b64c3d928ec79fe1a36801834e5950f2fb6f2171
Harsh Raj <rajharsha806@gmail.com>
2026-02-05 11:14:18 +0530

    to do kindoff

--- script.js
+++ script.js
@@ -29,22 +29,41 @@
 //     console.log("set time out")
 // },2000)
 
-let a= document.querySelector("p")
+// let a= document.querySelector("p")
 
-a.innerHTML="<h2>hi</h2>"
-a.style.backgroundColor="Red"
+// a.style.backgroundColor="Red"
 
-let b= document.querySelector("body")
-let btn=document.querySelector("button")
-let cur =0
-btn.addEventListener("click",function(){
-    if (cur==0){
-    b.style.backgroundColor="Black"
-    b.style.color="White"
-    cur=1
-    }else{
-        cur=0
-        b.style.backgroundColor="White"
-        b.style.color="Black"
+// let b= document.querySelector("body")
+// let btn=document.querySelector("button")
+// let cur =0
+// btn.addEventListener("click",function(){
+//     if (cur==0){
+//     b.style.backgroundColor="Black"
+//     b.style.color="White"
+//     a.style.backgroundColor="#aca8a8"
+//         cur=1
+//     }else{
+//         cur=0
+//         b.style.backgroundColor="White"
+//         b.style.color="Black"
+//         a.style.backgroundColor="Red"
+//     }
+// })
+
+let input=document.querySelector("#input1")
+let btn = document.querySelector("button")
+let list= document.querySelector("ol")
+
+btn.addEventListener("click",addNews)
+
+function addNews(){
+    let takedata= input.value;
+    if(takedata==""){
+        alert("Please enter news!")
+        return
     }
-})
+    let li=document.createElement("li")
+    li.innerText=takedata;
+    list.appendChild(li)
+    input.value=""
+}
