5e8ac3361adfb3985b8b40b5643bb042ed0e2142
Harsh Raj <rajharsha806@gmail.com>
2026-02-05 10:07:22 +0530

    js

---
+++ script.js
@@ -0,0 +1,25 @@
+// function 
+ //argument
+
+function display(name){
+    //parameter
+    name();
+}
+
+function ret(name){
+    return "hi"
+}
+console.log(ret());
+
+function greet(){
+    console.log("welcome")
+}
+display(greet)
+
+//arrow function
+a();
+var a=()=>{
+    console.log("hi")
+}
+console.log(a)
+
