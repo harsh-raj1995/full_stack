ba8d09404775abef5ca32b6e0c56e2f77dfc8716
Harsh Raj <rajharsha806@gmail.com>
2026-09-16 18:34:05 +0530

    todo- got delete update

--- vite-project/src/Items.jsx
+++ vite-project/src/Items.jsx
@@ -1,20 +1,31 @@
+import React from "react";
 import { useState } from "react";
 
 function Items(props){
   const [status,setStatus]= useState(props.item.status);
-
+  const [d,setDelete]= useState(false);
+  console.log(props.item.id);
+  if(d){
+    return <></>;
+  }else{
   return (
     <>
     <br></br>
     <div className="Card">
       
       <div className="List">{props.item.task}</div>
+      <div className="buttons">
       <button  onClick={()=>{
         setStatus(!status)
       }} className={`${status?"completed":"pending"}`}>{status?"Completed":"Pending"}</button>
+      <button onClick={()=>{
+        setDelete(true);
+      }} >Delete</button>
+      </div>
     </div>
     </>
 
   )
+  }
 }
-export default Items;
+export default React.memo(Items);
